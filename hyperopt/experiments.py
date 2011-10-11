da34de7bf3db217bbd07240f09b29d79c7ae25eb
James Bergstra <james.bergstra@gmail.com>
2011-10-11 18:47:25 -0400

    changing hyperopt-search to use experiments.main_search

--- hyperopt/experiments.py
+++ hyperopt/experiments.py
@@ -8,8 +8,12 @@ __copyright__ = "(c) 2010, Universite de Montreal"
 __license__   = "3-clause BSD License"
 __contact__   = "James Bergstra <pylearn-dev@googlegroups.com>"
 
+import cPickle
 import logging
+import os
+
 import base
+import utils
 
 logger = logging.getLogger(__name__)
 
@@ -31,3 +35,54 @@ class SerialExperiment(base.Experiment):
             logger.debug('result: %s' % str(result))
             self.trials.append(trial)
             self.results.append(result)
+
+
+def main_search():
+    from optparse import OptionParser
+    parser = OptionParser(
+            usage="%prog [options] [<bandit> <bandit_algo>]")
+    parser.add_option('--load',
+            default='experiment.pkl',
+            dest="load",
+            metavar='FILE',
+            help="unpickle experiment from here on startup")
+    parser.add_option('--save',
+            default='experiment.pkl',
+            dest="save",
+            metavar='FILE',
+            help="pickle experiment to here on exit")
+    parser.add_option("--steps",
+            dest='steps',
+            default=100,
+            metavar='N',
+            help="exit after queuing this many jobs (default: 100)")
+    parser.add_option("--workdir",
+            dest="workdir",
+            default=os.path.expanduser('~/.hyperopt.workdir'),
+            help="create workdirs here",
+            metavar="DIR")
+
+    (options, args) = parser.parse_args()
+    try:
+        bandit_json, bandit_algo_json = args
+    except:
+        parser.print_help()
+        return -1
+
+    try:
+        if not options.load:
+            raise IOError()
+        handle = open(options.load, 'rb')
+        self = cPickle.load(handle)
+        handle.close()
+    except IOError:
+        bandit = utils.json_call(bandit_json)
+        bandit_algo = utils.json_call(bandit_algo_json)
+        bandit_algo.set_bandit(bandit)
+        self = SerialExperiment(bandit, bandit_algo)
+
+    try:
+        self.run(options.steps)
+    finally:
+        if options.save:
+            cPickle.dump(self, open(options.save, 'wb'))
