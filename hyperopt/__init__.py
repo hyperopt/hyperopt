c2f1105c5dae0697df8013d3c196aa0920940143
James Bergstra <james.bergstra@gmail.com>
2012-02-21 19:14:16 -0500

    adding JOB_STATES to toplevel

--- hyperopt/__init__.py
+++ hyperopt/__init__.py
@@ -5,8 +5,17 @@
 
 
 from base import STATUS_STRINGS
-from base import (STATUS_NEW, STATUS_RUNNING, STATUS_SUSPENDED, STATUS_OK,
-        STATUS_FAIL)
+from base import STATUS_NEW
+from base import STATUS_RUNNING
+from base import STATUS_SUSPENDED
+from base import STATUS_OK
+from base import STATUS_FAIL
+
+from base import JOB_STATES
+from base import JOB_STATE_NEW
+from base import JOB_STATE_RUNNING
+from base import JOB_STATE_DONE
+from base import JOB_STATE_ERROR
 
 from base import Bandit
 from base import BanditAlgo
