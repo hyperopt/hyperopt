6db50f7a636cd2863297c49023452bd849ef123f
James Bergstra <james.bergstra@gmail.com>
2012-02-28 00:17:47 -0500

    @yamins81 - is this correct for dealing with scalars?

--- hyperopt/mongoexp.py
+++ hyperopt/mongoexp.py
@@ -698,8 +698,10 @@ class MongoTrials(Trials):
                 #concentrating on data in db and existing for state changes 
                 db_data = db_data[db_in_existing]
                 existing_data = existing_data[existing_in_db]
-                assert (existing_data['_id'] == db_data['_id']).all()
-                assert (existing_data['version'] <= db_data['version']).all()
+                assert numpy.asarray(
+                        existing_data['_id'] == db_data['_id']).all()
+                assert numpy.asarray(
+                        existing_data['version'] <= db_data['version']).all()
                 same_version = existing_data['version'] == db_data['version']
                 _trials = [_trials[_ind] for _ind in same_version.nonzero()[0]]
                 version_changes = existing_data[numpy.invert(same_version)]
