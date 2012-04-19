3f53a6f4cd81ac94e94ebf3c379fdab73158b111
James Bergstra <james.bergstra@gmail.com>
2012-04-19 10:39:26 -0400

    put 1% floor on the sigma of the parzen distributions

--- hyperopt/tpe.py
+++ hyperopt/tpe.py
@@ -452,7 +452,7 @@ def adaptive_parzen_normal(mus, prior_weight, prior_mu, prior_sigma,
 
     # -- magic formula:
     maxsigma = prior_sigma / 1.0
-    minsigma = prior_sigma / (1.0 + len(srtd_mus))
+    minsigma = prior_sigma / min(100, (1.0 + len(srtd_mus)))
 
     #print 'maxsigma, minsigma', maxsigma, minsigma
     sigma = np.clip(sigma, minsigma, maxsigma)
