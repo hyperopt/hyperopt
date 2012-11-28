657c530c6c51a4f36ca96b8025978658bedebf2f
James Bergstra <james.bergstra@gmail.com>
2012-11-27 22:05:56 -0500

    FIX: Ctrl.checkpoint should save to trials, even in toy serial situations

--- hyperopt/base.py
+++ hyperopt/base.py
@@ -603,7 +603,10 @@ class Ctrl(object):
         self.current_trial = current_trial
 
     def checkpoint(self, r=None):
-        warnings.warn('Ctrl.checkpoint base implementation is no-op')
+        # warnings.warn('Ctrl.checkpoint base implementation is no-op')
+        assert self.current_trial in self.trials._trials
+        if r is not None:
+            self.current_trial['result'] = r
 
     @property
     def attachments(self):
