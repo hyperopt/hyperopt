a7b57b8ed9a208f89a9e565bc9a076e0c0208c2b
James Bergstra <james.bergstra@gmail.com>
2013-05-27 16:22:51 -0400

    reorganizing pchoice testing logic into unit tests

--- hyperopt/tests/test_pchoice.py
+++ hyperopt/tests/test_pchoice.py
@@ -1,39 +1,54 @@
-# Not unit testing, just some random small tests.
-
+import numpy as np
+import unittest
 from sklearn import datasets
 from hyperopt import hp
-
-iris = datasets.load_iris()
-X, y = iris.data, iris.target
-X, y = X[y != 0, :2], y[y != 0]
-X_og, y_og = X, y
-
-space = hp.pchoice('naive_type', [.14, .02, .84],
-                   ['gaussian', 'multinomial', 'bernoulli'])
 import hyperopt.pyll.stochastic
-a, b, c = 0, 0, 0
-for i in range(0, 1000):
-    nesto = hyperopt.pyll.stochastic.sample(space)
-    if nesto == 'gaussian':
-        a += 1
-    elif nesto == 'multinomial':
-        b += 1
-    else:
-        c += 1
-print(a, b, c)
 
 
-space = hp.choice('normal_choice', [
-    hp.pchoice('fsd', [.1, .8, .1], ['first', 'second', 2]),
-    hp.choice('something_else', [10, 20])
-])
-a, b, c = 0, 0, 0
-for i in range(0, 1000):
-    nesto = hyperopt.pyll.stochastic.sample(space)
-    if nesto == 'first':
-        a += 1
-    elif nesto == 'second':
-        b += 1
-    elif nesto == 2:
-        c += 1
-print(a, b, c)
+class TestPChoice(unittest.TestCase):
+    def setUp(self):
+
+        iris = datasets.load_iris()
+        X, y = iris.data, iris.target
+        self.X, self.y = X[y != 0, :2], y[y != 0]
+        X_og, y_og = X, y
+
+    def test_basic(self):
+
+        space = hp.pchoice('naive_type', [.14, .02, .84],
+                           ['gaussian', 'multinomial', 'bernoulli'])
+        a, b, c = 0, 0, 0
+        rng = np.random.RandomState(123)
+        for i in range(0, 1000):
+            nesto = hyperopt.pyll.stochastic.sample(space, rng=rng)
+            if nesto == 'gaussian':
+                a += 1
+            elif nesto == 'multinomial':
+                b += 1
+            elif nesto == 'bernoulli':
+                c += 1
+            else:
+                assert 0, nesto
+        print(a, b, c)
+        assert 120 < a < 160
+        assert 0 < b < 40
+        assert 800 < c < 900
+
+    def test_basic2(self):
+        space = hp.choice('normal_choice', [
+            hp.pchoice('fsd', [.1, .8, .1], ['first', 'second', 2]),
+            hp.choice('something_else', [10, 20])
+        ])
+        a, b, c = 0, 0, 0
+        rng=np.random.RandomState(123)
+        for i in range(0, 1000):
+            nesto = hyperopt.pyll.stochastic.sample(space, rng=rng)
+            if nesto == 'first':
+                a += 1
+            elif nesto == 'second':
+                b += 1
+            elif nesto == 2:
+                c += 1
+        print(a, b, c)
+        assert b > 2 * a
+        assert b > 2 * c
