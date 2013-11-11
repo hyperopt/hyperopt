813e416067543a54bbece7756f4b112f3bee2846
James Bergstra <james.bergstra@gmail.com>
2013-11-11 18:41:04 -0500

    pep8 in rand.py

--- hyperopt/rand.py
+++ hyperopt/rand.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 def suggest(new_ids, domain, trials, seed):
-    logger.debug("in suggest with seed: %s"%(str(seed)))
+    logger.debug("in suggest with seed: %s" % (str(seed)))
     logger.debug('generating trials for new_ids: %s' % str(new_ids))
 
     # XXX -- Modifying domain by side-effect is bad form :/
@@ -28,16 +28,18 @@ def suggest(new_ids, domain, trials, seed):
             rng.seed(seed + ii)
             assert domain.rng is rng
             # -- sample new specs, idxs, vals
-            idxs, vals = pyll.rec_eval(domain.s_idxs_vals,
-                    memo={domain.s_new_ids: [new_id]})
+            idxs, vals = pyll.rec_eval(
+                domain.s_idxs_vals,
+                memo={domain.s_new_ids: [new_id]})
             new_result = domain.new_result()
             new_misc = dict(tid=new_id, cmd=domain.cmd, workdir=domain.workdir)
             miscs_update_idxs_vals([new_misc], idxs, vals)
             rval.extend(trials.new_trial_docs([new_id],
-                    [None], [new_result], [new_misc]))
+                        [None], [new_result], [new_misc]))
     finally:
         if domain_rng is not None:
             domain.rng = domain_rng
             domain.install_rng_in_s_idxs_vals(domain_rng)
     return rval
 
+# flake8 likes no trailing blank line
