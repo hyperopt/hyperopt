ba462b8bd3a4be8d258d2cdd1b885814d73e799a
Max Pumperla <max.pumperla@googlemail.com>
2016-11-12 20:00:05 +0100

    absolute imports for init files

--- hyperopt/pyll/__init__.py
+++ hyperopt/pyll/__init__.py
@@ -1,13 +1,13 @@
-from base import Apply
-from base import Literal
-from base import as_apply
-from base import scope
-from base import rec_eval
-from base import clone
-from base import clone_merge
-from base import dfs
-from base import toposort
+from __future__ import absolute_import
+from .base import Apply
+from .base import Literal
+from .base import as_apply
+from .base import scope
+from .base import rec_eval
+from .base import clone
+from .base import clone_merge
+from .base import dfs
+from .base import toposort
 
 # -- adds symbols to scope
-import stochastic
-
+from . import stochastic
