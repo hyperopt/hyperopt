18b55d11be42e6c82100222a12ed2a7419264ee3
Max Pumperla <max.pumperla@googlemail.com>
2016-11-13 11:58:30 +0100

    properly import zip

--- hyperopt/mix.py
+++ hyperopt/mix.py
@@ -1,4 +1,6 @@
+from builtins import zip
 import numpy as np
+
 
 def suggest(new_ids, domain, trials, seed, p_suggest):
     """Return the result of a randomly-chosen suggest function
@@ -22,14 +24,13 @@ def suggest(new_ids, domain, trials, seed, p_suggest):
         Make a suggestion from one of the suggest functions,
         in proportion to its corresponding probability.
         sum(probabilities) must be [close to] 1.0
-        
+
     """
     rng = np.random.RandomState(seed)
-    ps, suggests = zip(*p_suggest)
+    ps, suggests = list(zip(*p_suggest))
     assert len(ps) == len(suggests) == len(p_suggest)
     if not np.isclose(sum(ps), 1.0):
         raise ValueError('Probabilities should sum to 1', ps)
     idx = rng.multinomial(n=1, pvals=ps).argmax()
     return suggests[idx](new_ids, domain, trials,
                          seed=int(rng.randint(2 ** 31)))
-
