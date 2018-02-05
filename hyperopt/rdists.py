1aa439ee5e78a952108f4feb5954dc42b748d7f7
James Bergstra <james.bergstra@gmail.com>
2018-02-04 21:45:01 -0500

    fix lognormal tests

--- hyperopt/rdists.py
+++ hyperopt/rdists.py
@@ -41,69 +41,27 @@ class loguniform_gen(rv_continuous):
         return old_div((np.log(x) - self._low), (self._high - self._low))
 
 
-# -- cut and paste from scipy.stats
-#    because the way s is passed to these functions makes it impossible
-#    to construct this class. insane
-class lognorm_gen(rv_continuous):
-    """A lognormal continuous random variable.
+from scipy.stats._continuous_distns import lognorm_gen as scipy_lognorm_gen
 
-    %(before_notes)s
-
-    Notes
-    -----
-    The probability density function for `lognorm` is::
-
-        lognorm.pdf(x, s) = 1 / (s*x*sqrt(2*pi)) * exp(-1/2*(log(x)/s)**2)
-
-    for ``x > 0``, ``s > 0``.
-
-    If log x is normally distributed with mean mu and variance sigma**2,
-    then x is log-normally distributed with shape paramter sigma and scale
-    parameter exp(mu).
-
-    %(example)s
-
-    """
-
+class lognorm_gen(scipy_lognorm_gen):
     def __init__(self, mu, sigma):
         self.mu_ = mu
         self.s_ = sigma
-        self.norm_ = scipy.stats.norm
-        rv_continuous.__init__(self, a=0.0, name='loguniform', shapes='s')
+        scipy_lognorm_gen.__init__(self)
 
-    def _rvs(self):
-        s = self.s_
-        return np.exp(self.mu_ + s * self.norm_.rvs(size=self._size))
+        # I still don't understand what scipy stats objects are
+        # doing re: this stuff
+        del self.__dict__['_parse_args']
+        del self.__dict__['_parse_args_stats']
+        del self.__dict__['_parse_args_rvs']
 
-    def _pdf(self, x):
-        s = self.s_
-        Px = np.exp(old_div(-(np.log(x) - self.mu_) ** 2, (2 * s ** 2)))
-        return old_div(Px, (s * x * np.sqrt(2 * np.pi)))
-
-    def _cdf(self, x):
-        s = self.s_
-        return self.norm_.cdf(old_div((np.log(x) - self.mu_), s))
-
-    def _ppf(self, q):
-        s = self.s_
-        return np.exp(s * self.norm_._ppf(q) + self.mu_)
-
-    def _stats(self):
-        if self.mu_ != 0.0:
-            raise NotImplementedError()
-        s = self.s_
-        p = np.exp(s * s)
-        mu = np.sqrt(p)
-        mu2 = p * (p - 1)
-        g1 = np.sqrt((p - 1)) * (2 + p)
-        g2 = np.polyval([1, 2, 3, 0, -6.0], p)
-        return mu, mu2, g1, g2
-
-    def _entropy(self):
-        if self.mu_ != 0.0:
-            raise NotImplementedError()
-        s = self.s_
-        return 0.5 * (1 + np.log(2 * np.pi) + 2 * np.log(s))
+    def _parse_args(self, *args, **kwds):
+        assert not args, args
+        assert not kwds, kwds
+        args = (self.s_,)
+        loc = 0
+        scale = np.exp(self.mu_)
+        return args, loc, scale
 
 
 def qtable_pmf(x, q, qlow, xs, ps):
