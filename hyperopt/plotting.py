a6c2455f6cc3852abcbd658299d3e50323f6308c
Maximilian Ernestus <maximilian@ernestus.de>
2018-06-10 16:38:38 +0200

    Add plotting function to plot 1D trial attachments (e.g. learning progress)

--- hyperopt/plotting.py
+++ hyperopt/plotting.py
@@ -3,6 +3,9 @@ Functions to visualize an Experiment.
 
 """
 from __future__ import print_function
+
+import pickle
+
 from past.builtins import xrange
 try:
     unicode = unicode
@@ -79,7 +82,7 @@ def main_plot_histogram(trials, do_show=True, title="Loss Histogram"):
 
 def main_plot_vars(trials, do_show=True, fontsize=10,
                    colorize_best=None,
-                   columns=5,
+                   columns=5, arrange_by_loss=False
                    ):
     # -- import here because file-level import is too early
     import matplotlib.pyplot as plt
@@ -139,7 +142,11 @@ def main_plot_vars(trials, do_show=True, fontsize=10,
         plt.xticks(ticks_num, [''] * len(ticks_num))
 
         dist_name = label
-        x = idxs[label]
+
+        if arrange_by_loss:
+            x = [loss_by_tid[ii] for ii in idxs[label]]
+        else:
+            x = idxs[label]
         if 'log' in dist_name:
             y = np.log(vals[label])
         else:
@@ -153,4 +160,60 @@ def main_plot_vars(trials, do_show=True, fontsize=10,
             plt.yticks(nums, ['%.2e' % np.exp(t) for t in nums])
 
     if do_show:
+        plt.show()
+
+
+def main_plot_1D_attachment(trials, attachment_name, do_show=True, colorize_by_loss = True, max_darkness = 0.5,
+                            num_trails=None, preprocessing_fn=lambda x: x, line_width = 0.1):
+    """
+    Plots trail attachments, which are 1D-Data.
+
+    A legend is only added if the number of plotted elements is < 10.
+
+    :param trials: The trials object to gather the attachments from.
+    :param attachment_name: Thename of the attachment to gather.
+    :param do_show: If the plot should be shown after creating it.
+    :param colorize_by_loss: If the lines represening the trial data should be shaded by loss.
+    :param max_darkness: The maximumg shading darkness (between 0 and 1). Implies colorize_by_loss=True
+    :param num_trails: The number of trials to plot the attachment for. If none, all trials with a coresponding
+    attachment are taken. If set to any integer value, the trials are sorted by loss and trials are selected in regular
+    intervals for plotting. This ensures, that all possible outcomes are equally represented.
+    :param preprocessing_fn: A preprocessing funciton to be appleid to the attachment before plotting.
+    :param line_width: The width of the lines to be plotted.
+    :return: None
+    """
+    # -- import here because file-level import is too early
+    import matplotlib.pyplot as plt
+
+
+    plt.title(attachment_name)
+
+    min_loss = min(l for l in trials.losses() if l is not None)
+    max_loss = max(l for l in trials.losses() if l is not None)
+
+    if num_trails is None:
+        plotted_trials = trials
+    else:
+        trials_by_loss = sorted(filter(lambda t: 'loss' in t['result'], trials), key=lambda t: t['result']['loss'])
+        plotted_trials = [trials_by_loss[i] for i in np.linspace(0, len(trials_by_loss), num_trails, endpoint=False, dtype=np.int)]
+
+
+    for trial in plotted_trials:
+        t_attachments = trials.trial_attachments(trial)
+        if attachment_name in t_attachments:
+            attachment_data = np.squeeze(np.asanyarray(pickle.loads(t_attachments[attachment_name])))
+            if len(attachment_data.shape) == 1:
+                attachment_data = preprocessing_fn(attachment_data)
+                if colorize_by_loss:
+                    color = (0., 0., 0., max_darkness*(trial['result']['loss'] - min_loss)/(max_loss-min_loss))
+                else:
+                    color = None
+                plt.plot(attachment_data, color=color, linewidth=line_width,
+                         label="loss: {:.5}".format(trial['result']['loss']))
+            else:
+                pass #TODO: warn about the skipping
+
+    if do_show:
+        if len(plotted_trials) < 10:
+            plt.legend()
         plt.show()
