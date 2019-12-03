8816033b8f54928d5576b1495a6b8d6180e95fbd
Christian Lundgren <chrisavl@users.noreply.github.com>
2019-12-03 13:13:52 +0900

    Remove test incompatible with python2.7

--- hyperopt/tests/test_progress.py
+++ hyperopt/tests/test_progress.py
@@ -1,6 +1,4 @@
-from __future__ import print_function
-from builtins import range
-from unittest.mock import patch
+from future import standard_library
 import sys
 import io
 
@@ -14,13 +12,3 @@ def test_tqdm_progress_callback_restores_stdout():
         ctx.postfix = "best loss: 4711"
         ctx.update(42)
     assert sys.stdout == real_stdout
-
-
-def test_no_progress_callback_no_output():
-    output = io.StringIO()
-    with patch("sys.stdout", new=output) as stdout, no_progress_callback(
-        initial=0, total=100
-    ) as ctx:
-        ctx.postfix = "best loss: 4711"
-        ctx.update(42)
-    assert output.getvalue() == ""
