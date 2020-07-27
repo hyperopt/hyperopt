92ada8320fd886693eb02fd8b23846eb3638d493
Rohan Anand <thededlier@gmail.com>
2020-07-27 22:01:33 +0530

    set random seed in test

--- hyperopt/tests/test_atpe_basic.py
+++ hyperopt/tests/test_atpe_basic.py
@@ -1,29 +1,38 @@
 from __future__ import absolute_import
+import random
+import numpy as np
 from hyperopt import hp, fmin, atpe, space_eval
 
+random.seed(1)
+np.random.seed(1)
 
-class TestATPE:
-    def test_run_basic_search(self):
-        def objective(args):
-            case, val = args
-            if case == "case 2":
-                return val
-            else:
-                return val ** 2
 
-        # define a search space
-        space = hp.choice(
-            "a",
-            [
-                ("case 1", 1 + hp.lognormal("c1", 0, 1)),
-                ("case 2", hp.uniform("c2", -10, 10)),
-            ],
-        )
+def test_run_basic_search():
+    def objective(args):
+        case, val = args
+        if case == "case 1":
+            return val
+        else:
+            return val ** 2
 
-        # minimize the objective over the space
-        # NOTE: Max evals should be greater than 10, as the first 10 runs are only the initialization rounds
-        best = fmin(objective, space, algo=atpe.suggest, max_evals=20)
+    # define a search space
+    space = hp.choice(
+        "a",
+        [
+            ("case 1", 1 + hp.lognormal("c1", 0, 1)),
+            ("case 2", hp.uniform("c2", -10, 10)),
+        ],
+    )
 
-        # Assert that case 2 was the best choice
-        assert best["a"] == 1
-        assert space_eval(space, best)[0] == "case 2"
+    # minimize the objective over the space
+    # NOTE: Max evals should be greater than 10, as the first 10 runs are only the initialization rounds
+    best = fmin(
+        objective,
+        space,
+        algo=atpe.suggest,
+        max_evals=20,
+        rstate=np.random.RandomState(1),
+    )
+
+    assert best["a"] == 1
+    assert space_eval(space, best)[0] == "case 2"
